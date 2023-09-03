bda6c87d55b1d03272f6a98b589425cf7f69d013
cthed2 <cristhian.ipanaque@gmail.com>
2023-09-02 23:29:51 -0500

    Cierra #9

--- src/styles/styles.ts
+++ src/styles/styles.ts
@@ -3,24 +3,16 @@ import { createGlobalStyle } from "styled-components";
 export const Styles = createGlobalStyle`
 
     @font-face {
-        font-family: "Motiva Sans Light";
-        src: url("/fonts/Motiva-Sans-Light.ttf") format("truetype");
+        font-family: "Arboria Medium";
+        src: url("/fonts/Arboria-Medium.ttf") format("truetype");
         font-style: normal;
     }
-
-    @font-face {
-        font-family: "Motiva Sans Bold";
-        src: url("/fonts/Motiva-Sans-Bold.ttf") format("truetype");
-        font-style: normal;
-    }
-
 
     body,
     html,
     a {
-        font-family: 'Motiva Sans Light', sans-serif;
+        font-family: 'Arboria Medium', sans-serif;
     }
-
 
     body {
         margin:0;
@@ -57,7 +49,7 @@ export const Styles = createGlobalStyle`
     h4,
     h5,
     h6 {
-        font-family: 'Motiva Sans Bold', serif;
+        font-family: 'Arboria Medium', serif;
         color: #18216d;
         font-size: 56px;
         line-height: 1.18;
@@ -110,3 +102,4 @@ export const Styles = createGlobalStyle`
         width: 300px !important;
     }
 `;
+
