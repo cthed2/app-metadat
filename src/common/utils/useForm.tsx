88d1a38d7ed677b7ec907ce8d3eb41f6e472dc21
cthed2 <cristhian.ipanaque@gmail.com>
2023-09-03 23:09:23 -0500

    Cierra #15

--- src/common/utils/useForm.tsx
+++ src/common/utils/useForm.tsx
@@ -18,7 +18,7 @@ export const useForm = (validate: any) => {
     event.preventDefault();
     setErrors(validate(values));
     // Your url for API
-    const url = "";
+    const url = "https://script.google.com/macros/s/AKfycbxnPkRutCmhH9UKraehHS_5By3bxhXYYl6TnD9GVaAzm1B_U8WEitD_JqVXoclIzjhq/exec";
     if (Object.keys(values).length === 3) {
       axios
         .post(url, {
